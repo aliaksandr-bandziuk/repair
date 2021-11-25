44ab0e5cb1f31eecd7540176282494a4ad489c40
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2021-11-26 00:30:55 +0300

    feat: add regex

--- js/script.js
+++ js/script.js
@@ -61,6 +61,24 @@ const changeHeader = () => {
 window.onscroll = changeHeader;
 changeHeader();
 
+// formValidation
+const formValidation = () => {
+  const formPhone = document.querySelectorAll('.form-phone'),
+        formName = document.querySelectorAll('.form-name');
+    formPhone.forEach((item) => {
+      item.addEventListener('input', (event) => {
+          event.target.value = event.target.value.replace(/[^0-9+]/g, '');
+      });
+    });
+
+    formName.forEach((item) => {
+      item.addEventListener('input', (event) => {
+          event.target.value = event.target.value.replace(/[^А-Я|а-я]/g, '');
+      });
+    });
+};
+formValidation();
+
 // accordion
 miscPolyfillsForIE();
 
